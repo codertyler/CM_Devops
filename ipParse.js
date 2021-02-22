9402ae513ac28ecf958b125287d4ac75e9d3ee02
Tyler <tyler@iMac.local>
2021-02-22 00:13:07 -0700

    parsed json and added promises

--- ipParse.js
+++ ipParse.js
@@ -1,20 +1,53 @@
-const axios = require("axios");
+'use strict'
+const AWS = require('aws-sdk');
+const https = require('https');
 
-//Setting request url into a variable
-const requestURL = "https://ip-ranges.atlassian.com/";
+//Assigning IP range JSON into a variable
+const ipRangesUrl = 'https://ip-ranges.atlassian.com/';
 
+//Get IP ranges from Atlassian
+const getIpRanges = () => {
+  return new Promise((resolve, reject) => {
+    https.get(ipRangesUrl, (response) => {
+      let data = '';
+      response.setEncoding('utf8');
+      response.on('data', (d) => { data += d;});
+      response.on('error', (e) => { return reject(e);});
+      response.on('end', () => {
+        return resolve(JSON.parse(data))
+      })
+    })
+  })
+}
 
-//Making axios call to parse the JSON response from the url
+//Convert the IP range objec to SG Ingress Rule
+const toRule = (rangeObject) => {
+  return {
+    FromPort: target.port,
+    ToPort: target.port, 
+    IpProtocol: target.port,
+    IpRanges: [
+      {
+        CidrIp: rangeObject.cidr
+      }
+    ]
+  };
+};
 
-axios({
-  method: "GET",
-  url: requestURL,
-  //Although the json responses are automatically parsed in axios, for clarity including the responseType
-
-  responseType: 'json',
-}).then(function (response) {
-  const ipRange = response.data;
-  console.log(ipRange)
-});
-
-
+// Add Ingress Rules to SG
+const addIngressRules = (ipRanges) => {
+  return new Promise((resolve, reject) => {
+    const rules = ipRanges.
+      items.
+      filter((i) => {
+        return i.service === target.service && i.region === target.region;
+      }).
+      map(toRule);
+    const params = { GroupId: target.securityGroupId, IpPermissions: rules };
+    console.log(`Adding ${rules.length} ingress rules (as one rule)`);
+    ec2.authorizeSecurityGroupIngress(params, (err, data) => {
+      if (err) return reject(err);
+      return resolve();
+    });
+  });
+};
